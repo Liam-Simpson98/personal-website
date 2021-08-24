7eb00fef8132ce2efca66886097959c9fe223e9b
Liam Simpson <liamrsimpson0319@gmail.com>
2021-08-23 19:18:57 -0600

    Added Home page

---
+++ src/components/heroSection/HeroSection.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Button } from '../button/Button';
+import './HeroSection.css';
+import '../../App.css';
+
+const HeroSection = () => {
+  return (
+    <div className='hero-container'>
+      <video
+        src={require('./videos/video-2.mp4').default}
+        autoPlay
+        loop
+        muted
+      />
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+        >
+          WATCH TRAILER
+          <i className='far fa-play-circle' />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '../button/Button';
 import './Navbar.css';
@@ -18,14 +18,19 @@ function Navbar() {
     }
   };
 
+  useEffect(() => {
+    showButton();
+  }, []);
+
   window.addEventListener('resize', showButton);
 
   return (
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <Link to='/' className='navbar-logo'>
-            LIAM <i class='fas fa-code' />
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            LIAM
+            <i className='fas fa-code' style={{ marginLeft: 10 }} />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
@@ -44,24 +49,24 @@ function Navbar() {
               >
                 Services
               </Link>
-              <li className='nav-item'>
-                <Link
-                  to='/products'
-                  className='nav-links'
-                  onClick={closeMobileMenu}
-                >
-                  Products
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link
-                  to='/sign-up'
-                  className='nav-links-mobile'
-                  onClick={closeMobileMenu}
-                >
-                  Sign up
-                </Link>
-              </li>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/products'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Products
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Sign up
+              </Link>
             </li>
           </ul>
           {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
